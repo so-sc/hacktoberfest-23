5cfa12fc7d7d36909db32e6c3db676ab50ad0cfe
Deveesh Shetty <deveeshshetty@gmail.com>
2023-09-05 09:00:20 +0530

    adds site metadata

---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "Hacktoberfest 2023",
+  url: "https://hacknight.sosc.org.in",
+  ogImage: "https://hacknight.sosc.org.in/og.jpg",
+  description: "Hack Night is a flagship event organized by Sahyadri Open Source Community (SOSC) in celebration of Hacktoberfest 2023",
+  links: {
+    twitter: "https://twitter.com/sahyadri_osc",
+    github: "https://github.com/so-sc/hacktoberfest-23",
+  },
+}
