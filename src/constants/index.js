f47c4a8775381a2b8ea6962afb88964c68276ed4
srajan <kumarsrajan02@gmail.com>
2023-10-05 09:40:55 +0530

    adds flowchart

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+const events = [
+  {
+    title: "Title 1",
+    description: "Description",
+    icon: `/assets/icons/06@1x.png`,
+    iconBg: "#0f0913",
+    date: "28 October - 12:00 p.m",
+    points: ["First Point", "Second Point", "Third Point", "Fourth Point"],
+  },
+  {
+    title: "Title 2",
+    description: "Description",
+    icon: `/assets/icons/06@1x.png`,
+    iconBg: "#0f0913",
+    date: "28 October - 12:00 p.m",
+    points: ["First Point", "Second Point", "Third Point", "Fourth Point"],
+  },
+  {
+    title: "Title 3",
+    description: "Description",
+    icon: `/assets/icons/06@1x.png`,
+    iconBg: "#0f0913",
+    date: "28 October - 12:00 p.m",
+    points: ["First Point", "Second Point", "Third Point", "Fourth Point"],
+  },
+];
+
+export { events };
