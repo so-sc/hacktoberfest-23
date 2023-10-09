91a04bc881be500a17e2a05bcb4f7205bd276f43
srajan <kumarsrajan02@gmail.com>
2023-10-09 21:01:33 +0530

    testing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,45 @@
+// import React from "react";
+// import Link from "@/components/Links";
+// const Navbar = () => {
+//   return (
+//     <div className="w-full sticky top-0 z-50">
+//       <ul className="flex flex-wrap items-center font-jbExtrabold tracking-wider md:text-lg justify-between px-[10vw] md:px-[20vw] lg:px-[30vw] py-5">
+//         <li>
+//           <Link text="Home" href="#home" to=""></Link>
+//         </li>
+//         <li>
+//           <Link text="About" href="#about" to=""></Link>
+//         </li>
+//         <li>
+//           <Link text="Flowchart" href="#flowchart" to=""></Link>
+//         </li>
+//         <li>
+//           <Link text="Themes" href="#themes" to=""></Link>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
 import React from "react";
-import Link from "@/components/Links";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="w-full sticky top-0 z-50">
       <ul className="flex flex-wrap items-center font-jbExtrabold tracking-wider md:text-lg justify-between px-[10vw] md:px-[20vw] lg:px-[30vw] py-5">
         <li>
-          <Link text="Home" href="#home" to=""></Link>
+          <Link href="#home">Home</Link>
         </li>
         <li>
-          <Link text="About" href="#about" to=""></Link>
+          <Link href="#about">About</Link>
         </li>
         <li>
-          <Link text="Flowchart" href="#flowchart" to=""></Link>
+          <Link href="#flowchart">Flowchart</Link>
         </li>
         <li>
-          <Link text="Themes" href="#themes" to=""></Link>
+          <Link href="#themes">Themes</Link>
         </li>
       </ul>
     </div>
