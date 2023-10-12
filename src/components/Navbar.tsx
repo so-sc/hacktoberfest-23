928de18bc8bc630ab2337e0d0d95eae379665008
srajan <kumarsrajan02@gmail.com>
2023-10-12 09:40:48 +0530

    test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,16 +5,16 @@ const Navbar = () => {
     <div className="w-full fixed top-0 z-50">
       <ul className="flex flex-wrap items-center font-jbExtrabold tracking-wider md:text-lg justify-between px-[10vw] md:px-[20vw] lg:px-[30vw] py-5">
         <li>
-          <Link text="Home" href="#home" to=""></Link>
+          <Link text="Home" href="#home" to="_self"></Link>
         </li>
         <li>
-          <Link text="About" href="#about" to=""></Link>
+          <Link text="About" href="#about" to="_self"></Link>
         </li>
         <li>
-          <Link text="Flowchart" href="#flowchart" to=""></Link>
+          <Link text="Flowchart" href="#flowchart" to="_self"></Link>
         </li>
         <li>
-          <Link text="Themes" href="#themes" to=""></Link>
+          <Link text="Themes" href="#themes" to="_self"></Link>
         </li>
       </ul>
     </div>
